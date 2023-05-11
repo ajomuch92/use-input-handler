cc640433b59ff8e9708c99371db5d676a76983f5
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-05-10 20:49:05 -0600

    docs: ✏️ Updating example project inside package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import useInputHandler from 'use-input-handler';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+  const [value, setValue] = useInputHandler('');
+  const [value2, setValue2] = useInputHandler(0, {
+    parser: (newValue: any) => parseFloat(newValue),
+    validator: (valueToValidate: any) => {
+      return typeof valueToValidate === 'number' && valueToValidate > 18;
+    },
+    onValidatorFail: () => {
+      setMessage('Age must be greater than 18 years old');
+    },
+    onValidatorSuccess: () => {
+      setMessage('');
+    },
+    allowNull: true,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <input type="text" onChange={setValue} placeholder="Any text..." />
+      <p>
+          Value: { value?.toString() }
       </p>
+      <input type="number" onChange={setValue2} placeholder="Age..." />
+      <p>
+          Age: { value2?.toString() }
+      </p>
+      <p>Message: { message }</p>
     </>
   )
 }
