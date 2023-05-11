0cf82ff952705f90b990a642dab205d4f472e270
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-05-10 19:34:08 -0600

    refactor: 💡 Changing parser returned value

--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,5 @@ export default interface Config {
   allowNull?: Boolean;
   validator?: (str: UseInputType) => Boolean | boolean;
   onValidatorFail?: Function;
-  parser?: <T>(str: String|string) => T;
+  parser?: (str: String|string) => UseInputType;
 }
