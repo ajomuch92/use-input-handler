47c01f913d0a90b718cde5b8631debb1fbb8c908
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-06-01 06:19:23 -0600

    feat: 🎸 Updating test used with new dynamic types

--- tests/component.tsx
+++ tests/component.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import useInputHandler, { Config } from '../src';
 
-const InputComponent = (props: Config = {}): React.JSX.Element => {
+interface InputComponentProps extends Config<string | number> {}
+
+const InputComponent: React.FC<InputComponentProps> = (props: InputComponentProps) => {
   const [value, onChangeHandler] = useInputHandler(props.asNumber ? 0 : '', props);
 
-  return (<input type="text" value={value?.toString()} onChange={onChangeHandler} />);
-}
+  return (
+    <input
+      type="text"
+      value={value?.toString()}
+      onChange={onChangeHandler}
+    />
+  );
+};
 
-const NumberInputComponent = (): React.JSX.Element => {
-  const [value, onChangeHandler] = useInputHandler(0, { asNumber : true});
+const NumberInputComponent: React.FC = () => {
+  const [value, onChangeHandler] = useInputHandler(0, { asNumber: true });
 
   return (
     <>
-      <input type="text" onChange={onChangeHandler} />
-      <p role="out" title={typeof value}></p>
+      <input type="text" value={value.toString()} onChange={onChangeHandler} />
+      <p role="out" title={typeof value}>{value}</p>
     </>
-  )
-}
+  );
+};
 
 export {
   InputComponent,
